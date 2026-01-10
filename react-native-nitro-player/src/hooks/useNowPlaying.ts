cfffa9cd4763b5fc341f87f2ffcbdc601af2071b
riteshshukla04 <riteshshukla2381@gmail.com>
2026-01-10 14:08:58 +0530

    chore:- track-player-hook

--- react-native-nitro-player/src/hooks/useNowPlaying.ts
+++ react-native-nitro-player/src/hooks/useNowPlaying.ts
@@ -4,24 +4,24 @@ import type { PlayerState } from '../types/PlayerQueue'
 
 /**
  * Hook to get the current player state (same as TrackPlayer.getState())
- * 
+ *
  * This hook provides all player state information including:
  * - Current track
  * - Current position and duration
  * - Playback state (playing, paused, stopped)
  * - Current playlist ID
  * - Current track index
- * 
+ *
  * The hook polls getState() periodically and also listens to events
  * for immediate updates when state changes.
- * 
+ *
  * @returns PlayerState object with all current player information
- * 
+ *
  * @example
  * ```tsx
  * function PlayerComponent() {
  *   const state = useNowPlaying()
- * 
+ *
  *   return (
  *     <View>
  *       {state.currentTrack && (
@@ -78,10 +78,6 @@ export function useNowPlaying(): PlayerState {
     TrackPlayer.onPlaybackStateChange(() => {
       updateState()
     })
-
-  
-
-
   }, [])
 
   return state
