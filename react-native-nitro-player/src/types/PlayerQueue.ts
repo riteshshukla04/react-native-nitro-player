bef8bf351dba4b78e861073a96a7753f610cfa3f
riteshshukla04 <riteshshukla2381@gmail.com>
2026-02-06 02:49:08 +0530

    chore: extraParams

--- react-native-nitro-player/src/types/PlayerQueue.ts
+++ react-native-nitro-player/src/types/PlayerQueue.ts
@@ -1,3 +1,5 @@
+import type { AnyMap } from "react-native-nitro-modules"
+
 export type CurrentPlayingType =
   | 'playlist'
   | 'up-next'
@@ -10,7 +12,8 @@ export interface TrackItem {
   album: string
   duration: number
   url: string
-  artwork?: string | null
+  artwork?: string | null,
+  extraPayload?: AnyMap
 }
 
 export interface Playlist {
