67d7ae912a678d1f6d8d2ec397a3c0c7fa568986
riteshshukla04 <riteshshukla2381@gmail.com>
2026-03-03 11:50:19 +0530

    fix: imports

--- react-native-nitro-player/android/src/main/cpp/cpp-adapter.cpp
+++ react-native-nitro-player/android/src/main/cpp/cpp-adapter.cpp
@@ -1,11 +1,11 @@
 #include "NitroPlayerOnLoad.hpp"
 
 #include <jni.h>
+#include <fbjni/fbjni.h>
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*) {
-  return margelo::nitro::nitroplayer::initialize(vm);
+  return facebook::jni::initialize(vm, []() {
+    margelo::nitro::nitroplayer::registerAllNatives();
+  });
 }
 
-return facebook::jni::initialize(vm, []() {
-  margelo::nitro::nitroplayer::registerAllNatives();
-});
